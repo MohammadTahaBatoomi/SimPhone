99757463dbefed9181b80db16c88bd72556707bf
Mohammad Taha <tbatoomi@gmail.com>
2025-08-20 17:20:17 +0330

    convert css to tailwinds

--- .next/server/chunks/ssr/[root-of-the-server]__55db75d8._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__55db75d8._.js
@@ -11,73 +11,87 @@ __turbopack_context__.n(__turbopack_context__.i("[project]/src/app/layout.tsx [a
 
 __turbopack_context__.s([
     "default",
-    ()=>__TURBOPACK__default__export__
+    ()=>Simulator
 ]);
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js [app-rsc] (ecmascript)");
 ;
+;
 function Simulator() {
-    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["Fragment"], {
+    return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
+        className: "flex items-center justify-center p-6",
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-            className: "iphone-13",
+            className: "jsx-c20273d774a53112" + " " + "relative w-[420px] h-[890px] rounded-[60px] border-[14px] border-black bg-black overflow-visible shadow-[0_0_20px_rgba(0,0,0,0.2)]",
             children: [
                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                    className: "notch"
+                    className: "jsx-c20273d774a53112" + " " + "absolute top-0 left-1/2 transform -translate-x-1/2 w-[150px] h-[35px] bg-black rounded-b-[22px] z-10"
                 }, void 0, false, {
                     fileName: "[project]/src/components/Simulator.tsx",
                     lineNumber: 7,
                     columnNumber: 9
                 }, this),
                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                    className: "power-button"
-                }, void 0, false, {
-                    fileName: "[project]/src/components/Simulator.tsx",
-                    lineNumber: 8,
-                    columnNumber: 9
-                }, this),
-                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                    className: "silent"
+                    className: "jsx-c20273d774a53112" + " " + "absolute right-[-17px] top-[180px] w-[10px] h-[100px] bg-[#424242] rounded-[4px] border border-[#424242] transition-all duration-300 ease-300 z-[-1] cursor-pointer"
                 }, void 0, false, {
                     fileName: "[project]/src/components/Simulator.tsx",
                     lineNumber: 9,
                     columnNumber: 9
                 }, this),
                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                    className: "volume-up"
-                }, void 0, false, {
-                    fileName: "[project]/src/components/Simulator.tsx",
-                    lineNumber: 10,
-                    columnNumber: 9
-                }, this),
-                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                    className: "volume-down"
+                    className: "jsx-c20273d774a53112" + " " + "absolute left-[-17px] top-[120px] w-[10px] h-[40px] bg-[#424242] rounded-[4px] border border-[#424242] transition-all duration-300 ease-300 z-[-1] cursor-pointer"
                 }, void 0, false, {
                     fileName: "[project]/src/components/Simulator.tsx",
                     lineNumber: 11,
                     columnNumber: 9
                 }, this),
                 /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-                    className: "indicator"
-                }, void 0, false, {
-                    fileName: "[project]/src/components/Simulator.tsx",
-                    lineNumber: 12,
-                    columnNumber: 9
-                }, this),
-                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("iframe", {
-                    src: "https://sabzlearn.ir/"
+                    className: "jsx-c20273d774a53112" + " " + "absolute left-[-17px] top-[180px] w-[10px] h-[60px] bg-[#424242] rounded-[4px] border border-[#424242] transition-all duration-300 ease-300 z-[-1] cursor-pointer"
                 }, void 0, false, {
                     fileName: "[project]/src/components/Simulator.tsx",
                     lineNumber: 13,
                     columnNumber: 9
-                }, this)
+                }, this),
+                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
+                    className: "jsx-c20273d774a53112" + " " + "absolute left-[-17px] top-[250px] w-[10px] h-[60px] bg-[#424242] rounded-[4px] border border-[#424242] transition-all duration-300 ease-300 z-[-1] cursor-pointer"
+                }, void 0, false, {
+                    fileName: "[project]/src/components/Simulator.tsx",
+                    lineNumber: 15,
+                    columnNumber: 9
+                }, this),
+                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
+                    className: "jsx-c20273d774a53112" + " " + "absolute bottom-[12px] left-1/2 transform -translate-x-1/2 w-[120px] h-[6px] bg-[#525253] rounded-[3px] opacity-80 transition-all duration-300 ease-300 cursor-pointer"
+                }, void 0, false, {
+                    fileName: "[project]/src/components/Simulator.tsx",
+                    lineNumber: 17,
+                    columnNumber: 9
+                }, this),
+                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("iframe", {
+                    src: "https://sabzlearn.ir/",
+                    title: "Device Preview",
+                    style: {
+                        scrollbarWidth: "none"
+                    },
+                    className: "jsx-c20273d774a53112" + " " + "sim-iframe w-full h-full border-none rounded-[48px] overflow-hidden"
+                }, void 0, false, {
+                    fileName: "[project]/src/components/Simulator.tsx",
+                    lineNumber: 20,
+                    columnNumber: 9
+                }, this),
+                /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(void 0, {
+                    id: "c20273d774a53112",
+                    children: ".sim-iframe::-webkit-scrollbar{display:none}.sim-iframe{-ms-overflow-style:none;scrollbar-width:none}"
+                }, void 0, false, void 0, this)
             ]
         }, void 0, true, {
             fileName: "[project]/src/components/Simulator.tsx",
             lineNumber: 6,
             columnNumber: 7
         }, this)
-    }, void 0, false);
+    }, void 0, false, {
+        fileName: "[project]/src/components/Simulator.tsx",
+        lineNumber: 5,
+        columnNumber: 5
+    }, this);
 }
-const __TURBOPACK__default__export__ = Simulator;
 }),
 "[project]/src/app/page.tsx [app-rsc] (ecmascript)", ((__turbopack_context__) => {
 "use strict";
--- src/components/Simulator.tsx
+++ src/components/Simulator.tsx
@@ -1,20 +1,37 @@
-import React from 'react'
+"use client"
 
-function Simulator() {
+import React from "react";
+
+export default function Simulator() {
   return (
-    <>
-      <div className="iphone-13">
-        <div className="notch"></div>
-        <div className="power-button"></div>
-        <div className="silent"></div>
-        <div className="volume-up"></div>
-        <div className="volume-down"></div>
-        <div className="indicator"></div>
-        <iframe src="https://sabzlearn.ir/"></iframe>
-      </div>
-   </>
-  
-  )
-}
+    <div className="flex items-center justify-center p-6">
+      <div className="relative w-[420px] h-[890px] rounded-[60px] border-[14px] border-black bg-black overflow-visible shadow-[0_0_20px_rgba(0,0,0,0.2)]">
+        <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[150px] h-[35px] bg-black rounded-b-[22px] z-10" />
 
-export default Simulator;
+        <div className="absolute right-[-17px] top-[180px] w-[10px] h-[100px] bg-[#424242] rounded-[4px] border border-[#424242] transition-all duration-300 ease-300 z-[-1] cursor-pointer" />
+
+        <div className="absolute left-[-17px] top-[120px] w-[10px] h-[40px] bg-[#424242] rounded-[4px] border border-[#424242] transition-all duration-300 ease-300 z-[-1] cursor-pointer" />
+
+        <div className="absolute left-[-17px] top-[180px] w-[10px] h-[60px] bg-[#424242] rounded-[4px] border border-[#424242] transition-all duration-300 ease-300 z-[-1] cursor-pointer" />
+
+        <div className="absolute left-[-17px] top-[250px] w-[10px] h-[60px] bg-[#424242] rounded-[4px] border border-[#424242] transition-all duration-300 ease-300 z-[-1] cursor-pointer" />
+
+        <div className="absolute bottom-[12px] left-1/2 transform -translate-x-1/2 w-[120px] h-[6px] bg-[#525253] rounded-[3px] opacity-80 transition-all duration-300 ease-300 cursor-pointer" />
+
+        {/* iframe - full screen of the device with rounded inner corners */}
+        <iframe
+          className="sim-iframe w-full h-full border-none rounded-[48px] overflow-hidden"
+          src="https://sabzlearn.ir/"
+          title="Device Preview"
+          style={{ scrollbarWidth: "none" }}
+        />
+
+        {/* hide scrollbars for webkit browsers and ensure iframe fills its container */}
+        <style jsx global>{`
+          .sim-iframe::-webkit-scrollbar { display: none; }
+          .sim-iframe { -ms-overflow-style: none; scrollbar-width: none; }
+        `}</style>
+      </div>
+    </div>
+  );
+}
