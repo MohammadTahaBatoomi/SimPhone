29b6672cf9b38db241ce481fdd5a6edfba9d7b23
MohammadTahaBatoomi <tbatoomi@gmail.com>
2025-09-10 12:02:22 +0330

    remove code styling

--- .next/static/chunks/src_ae770472._.js
+++ .next/static/chunks/src_ae770472._.js
@@ -79,34 +79,26 @@ __turbopack_context__.s([
     ()=>__TURBOPACK__default__export__
 ]);
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/compiled/react/jsx-dev-runtime.js [app-client] (ecmascript)");
-var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$styled$2d$jsx$2f$style$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/styled-jsx/style.js [app-client] (ecmascript)");
 var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$index$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__ = __turbopack_context__.i("[project]/node_modules/next/dist/compiled/react/index.js [app-client] (ecmascript)");
 "use client";
-;
 ;
 ;
 const IframeWrapper = /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$index$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["forwardRef"])(_c = (param, ref)=>{
     let { address, isLoading, onLoad } = param;
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
-        className: "jsx-eb60fe823c4f3d24" + " " + "w-full h-full relative overflow-hidden rounded-b-[48px] mt-[50px]",
-        children: [
-            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("iframe", {
-                ref: ref,
-                src: address,
-                title: "Device Preview",
-                onLoad: onLoad,
-                className: "jsx-eb60fe823c4f3d24" + " " + "sim-iframe absolute top-0 left-[-22px] w-[calc(100%+30px)] pb-20 h-[calc(100%+30px)] border-none rounded-b-[48px] ".concat(isLoading ? "hidden" : "")
-            }, void 0, false, {
-                fileName: "[project]/src/components/IframeWrapper.tsx",
-                lineNumber: 14,
-                columnNumber: 9
-            }, ("TURBOPACK compile-time value", void 0)),
-            /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$styled$2d$jsx$2f$style$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
-                id: "eb60fe823c4f3d24",
-                children: ".sim-iframe::-webkit-scrollbar{display:none}.sim-iframe{-ms-overflow-style:none;scrollbar-width:none}"
-            }, void 0, false, void 0, ("TURBOPACK compile-time value", void 0))
-        ]
-    }, void 0, true, {
+        className: "w-full h-full relative overflow-hidden rounded-b-[48px] mt-[50px]",
+        children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])("iframe", {
+            ref: ref,
+            className: "absolute top-0 left-[-22px] w-[calc(100%+30px)] pb-20 h-[calc(100%+30px)] border-none rounded-b-[48px] ".concat(isLoading ? "hidden" : ""),
+            src: address,
+            title: "Device Preview",
+            onLoad: onLoad
+        }, void 0, false, {
+            fileName: "[project]/src/components/IframeWrapper.tsx",
+            lineNumber: 14,
+            columnNumber: 9
+        }, ("TURBOPACK compile-time value", void 0))
+    }, void 0, false, {
         fileName: "[project]/src/components/IframeWrapper.tsx",
         lineNumber: 13,
         columnNumber: 7
--- src/components/IframeWrapper.tsx
+++ src/components/IframeWrapper.tsx
@@ -13,22 +13,13 @@ const IframeWrapper = forwardRef<HTMLIFrameElement, IframeWrapperProps>(
       <div className="w-full h-full relative overflow-hidden rounded-b-[48px] mt-[50px]">
         <iframe
           ref={ref}
-          className={`sim-iframe absolute top-0 left-[-22px] w-[calc(100%+30px)] pb-20 h-[calc(100%+30px)] border-none rounded-b-[48px] ${
+          className={`absolute top-0 left-[-22px] w-[calc(100%+30px)] pb-20 h-[calc(100%+30px)] border-none rounded-b-[48px] ${
             isLoading ? "hidden" : ""
           }`}
           src={address}
           title="Device Preview"
           onLoad={onLoad}
         />
-        <style jsx global>{`
-          .sim-iframe::-webkit-scrollbar {
-            display: none;
-          }
-          .sim-iframe {
-            -ms-overflow-style: none;
-            scrollbar-width: none;
-          }
-        `}</style>
       </div>
     );
   }
